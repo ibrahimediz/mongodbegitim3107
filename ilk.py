c3e4b583a4b32a7e1420404a4124ad6454a043ac
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-31 07:43:48 +0000

    31072023_2

--- ilk.py
+++ ilk.py
@@ -1,6 +1,9 @@
 liste = ["AliGalip","Bedirhan","Emsal","EnesAlper","HilalAltinbas","Ilker","Melek","Melih","Muzeyyen","Necmi","Ozge","Sinan","Suat"]
 import os
+import shutil
 for item in liste:
     if not os.path.exists(os.path.join("Exercises",item)):
         os.mkdir(os.path.join("Exercises",item))
-        
+    source = "/workspace/mongodbegitim3107/Documents/ornek.js"
+    destination = os.path.join("Exercises",item,"ornek.js")
+    shutil.copy(source,destination)
