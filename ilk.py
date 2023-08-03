2a6f5a885ea1c48addb958941bd9756faef8b18c
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-08-03 06:51:32 +0000

    03082023_1

--- ilk.py
+++ ilk.py
@@ -11,7 +11,7 @@ import shutil
 for item in liste:
     if not os.path.exists(os.path.join("Exercises",item)):
         os.mkdir(os.path.join("Exercises",item))
-    fileName = f"{item}_updateMany.mongodb.js"
+    fileName = f"{item}_aggregate.mongodb.js"
     if not os.path.exists(os.path.join("workspace","mongodbegitim3107","Documents",fileName)):
         open(os.path.join("Exercises",item,fileName),"w+")
     else:
